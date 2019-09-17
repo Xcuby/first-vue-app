27ecf80683f6748b6a7294f3a3d04624b084351b
Xcuby <X.ALBANET@LAPOSTE.NET>
2019-09-17 11:21:07 +0200

    Premier test images

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,142 +1,47 @@
 <template>
-  <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
-
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
-
-      <v-flex
-        mb-5
-        xs12
-      >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
+  <v-container class="grey lighten-5">
+    <v-row justify:space arround>
+      <template v-for="n in 4">
+        <v-col :key="n">
+          <v-card class="pa-1" outlined tile elevation="12">
+            <v-img
+              :src="require('./assets/bugatti.jpg')"
+              aspect-ratio="1"
+              class="grey lighten-2"
+              max-width="800"
+              max-height="250"
+            ></v-img>
+            <div class="text-center font-weight-medium">Titre de la photo</div>
+            <v-card-text>Texte de la photo</v-card-text>
+          </v-card>
+        </v-col>
+        <v-responsive v-if="n === 2" :key="`width-${n}`" width="100%"></v-responsive>
+      </template>
+    </v-row>
+    <v-row justify:space arround>
+      <v-col md="10">
+        <v-text-field label="Entrez votre réponse" filled></v-text-field>
+      </v-col>
+      <div class="my-3">
+        <v-btn x-large color="primary">Valider</v-btn>
+      </div>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   data: () => ({
-    ecosystem: [
+    énoncé: [
+      { consigne: "Trouvez l'intrus parmis les images suivantes" },
+      { consigne: 'Entrez le points communs entre ces différentes images' },
       {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
+        consigne:
+          'A votre avis de quand date ces photos (elles ont toutes été prises à la même date)'
+      }
     ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
-};
+    image: [
+    ]
+  })
+}
 </script>
